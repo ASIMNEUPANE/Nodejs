91e1f2601fbf376322df23117cf302ae1d4ce471
ASIMNEUPANE <124865108+ASIMNEUPANE@users.noreply.github.com>
2023-08-01 21:04:28 +0545

    added new file

---
+++ script.js
@@ -0,0 +1,18 @@
+// Variables (var, let, const)
+// ES5
+var person = "ram";
+person = "shyam";
+
+console.log(person);
+
+// ES6
+const person1 = "ram";
+
+console.log({ person1 });
+
+// Let
+
+let person2 = "ram";
+person2 = "shyam";
+
+console.log({ person2 });
