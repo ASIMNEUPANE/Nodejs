fa0883d1582bb8be4cb083e6e2c1afa5732a321c
ASIMNEUPANE <124865108+ASIMNEUPANE@users.noreply.github.com>
2023-08-03 17:59:21 +0545

    learned function and object

--- script.js
+++ script.js
@@ -1,55 +1,76 @@
-// // Variables (var, let, const)
-// // ES5
-// var person = "ram";
-// person = "shyam";
+// // // Variables (var, let, const)
+// // // ES5
+// // var person = "ram";
+// // person = "shyam";
 
-// console.log(person);
+// // console.log(person);
 
-// // ES6
-// const person1 = "ram";
+// // // ES6
+// // const person1 = "ram";
 
-// console.log({ person1 });
+// // console.log({ person1 });
 
-// // Let
+// // // Let
 
-// let person2 = "ram";
-// person2 = "shyam";
+// // let person2 = "ram";
+// // person2 = "shyam";
 
-// console.log({ person2 });
+// // console.log({ person2 });
 
-// // logical
+// // // logical
 
-// let hour = promt("give a random number number")
-// if(hour>10 || hour<13){
-//     alert(" js clss is in session")
+// // let hour = promt("give a random number number")
+// // if(hour>10 || hour<13){
+// //     alert(" js clss is in session")
+// // }
+
+// // function guessGame(){
+// //     let guess = prompt("guess your number");
+// // let num = Math.floor(Math.random() * 10);
+
+
+// // if(guess===num){
+// //     alert("your guess is right")
+// // }
+// // if(guess>num){
+// //     alert("your guess is higher than the actual number")
+// // }
+// // if(guess<num){
+// //     alert("your guess is lesser than the actual number")
+// // }
+
+
+
+// // }
+
+// // guessGame()
+
+// // function 
+
+// const user =(name) =>{
+
+//     console.log(" hello " + name)
 // }
 
-function guessGame(){
-    let guess = prompt("guess your number");
-let num = Math.floor(Math.random() * 10);
+// user("Asim")
+// const helo = (a,b) =>{
 
-
-if(guess===num){
-    alert("your guess is right")
-}
-if(guess>num){
-    alert("your guess is higher than the actual number")
-}
-if(guess<num){
-    alert("your guess is lesser than the actual number")
-}
-
-
-
-}
-// do{
-//     alert("your answer is wrong")
-//     let guess = prompt("guess your number");
-//     let num = Math.floor(Math.random() * 10);
-    
+//     return a+b;
 // }
-// while(guess != num);
+
+// const result = helo(1,2)
+// console.log(result)
+
+
+// const add = (a,b)=>{
+// if (a<0 && b<0){
+//     return null;
+// }
+// return a+b;
 
 
 // }
-guessGame()
+// add(-1,2);
+
+const num = Number(prompt("enter your age"));
+console.log(num);
