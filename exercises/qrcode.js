b6019f5c1a58e7b9bdf05eb270d2903c8b3ba3a1
ASIMNEUPANE <124865108+ASIMNEUPANE@users.noreply.github.com>
2023-08-10 17:19:27 +0545

    generate rqcode in html

--- exercises/qrcode.js
+++ exercises/qrcode.js
@@ -1,8 +1,24 @@
 const QRCode = require('qrcode');
-const QRd = (url)=>{
-    QRCode.toDataURL('https://github.com/ASIMNEUPANE',    (err, url)=> {
-    console.log(url)
-  })}
+const http = require("http");
 
-  console.log(QRd);
-  
+
+
+const QRd = async(text)=> {
+  const generateQr =  await QRCode.toDataURL(text)
+return generateQr
+ }
+
+
+const ht = http.createServer(async (req,res)=>{
+
+  const data = await QRd("https://github.com/ASIMNEUPANE")
+
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  const html =  `<img src= "${data}">  </img>`;
+  res.end(html)
+
+}
+)
+
+
+ht.listen(3002)
