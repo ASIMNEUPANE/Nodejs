b3e2cefef93649e6f2d7b2809e21c2dc0b0f29d0
ASIMNEUPANE <124865108+ASIMNEUPANE@users.noreply.github.com>
2023-08-10 23:29:08 +0545

    install new package called axios

--- exercises/wheather.js
+++ exercises/wheather.js
@@ -1,9 +1,3 @@
+const axios = require('axios');
 
-const ax = require("axios");
-
-async function logMovies() {
-    const response = await fetch('https://api.open-meteo.com/v1/forecast?latitude=27.70&longitude=85.32&hourly=temperature_2m&daily=sunrise,sunset&forecast_days=1&timezone=auto')
-    const movies = await response.json();
-    console.log(movies);
-  }
-
+console.log(axios);
