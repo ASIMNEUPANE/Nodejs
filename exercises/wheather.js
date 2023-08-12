b11fdea21d9c441dcdc3c49012af67dc4a8456c9
ASIMNEUPANE <124865108+ASIMNEUPANE@users.noreply.github.com>
2023-08-12 13:33:40 +0545

    changes

--- exercises/wheather.js
+++ exercises/wheather.js
@@ -1,28 +1,60 @@
 const axios = require('axios');
 
-async function getUser(url) {
+async function getUser() {
     try {
-        const { data } = await axios.get('url');
+        const { data } = await axios.get( "https://api.open-meteo.com/v1/forecast?latitude=27.70&longitude=85.32&hourly=temperature_2m&daily=sunrise,sunset&forecast_days=1&timezone=auto"
+        );
         const { hourly } = data;
 
         const myFunction = () => {
             ({ hoursdata: hourly.time, hourlyTemp: hourly.temperature_2m })
 
-            return myFunction;
+            return hoursdata;
         }
+        console.log(myFunction());
 
-        //    const {name} = data;
-        //    return {sunrise: daily.sunrise, sunset: daily.sunset };
+
 
     } catch (error) {
         console.error(error);
     }
 }
 
-const solution = ()=>{
-const findTime = new Date().getHours();
-findTime.f
-}
+// const solution = ()=>{
+// const findTime = new Date().getHours();
+// findTime.f
+// }
 
-const url = "https://api.open-meteo.com/v1/forecast?latitude=27.70&longitude=85.32&hourly=temperature_2m&daily=sunrise,sunset&forecast_days=1&timezone=auto"
-getUser(url);
+// console.log(getUser());
+
+
+
+// const axios = require('axios');
+
+// async function getUser(url) {
+//     try {
+//         const { data } = await axios.get('url');
+//         const { hourly } = data;
+
+//         const myFunction = () => {
+//             ({ hoursdata: hourly.time, hourlyTemp: hourly.temperature_2m })
+
+//             return myFunction;
+//         }
+
+//         //    const {name} = data;
+//         //    return {sunrise: daily.sunrise, sunset: daily.sunset };
+
+//     } catch (error) {
+//         console.error(error);
+//     }
+// }
+
+// const solution = ()=>{
+// const findTime = new Date().getHours();
+// findTime.f
+// }
+
+// const url = "https://api.open-meteo.com/v1/forecast?latitude=27.70&longitude=85.32&hourly=temperature_2m&daily=sunrise,sunset&forecast_days=1&timezone=auto"
+// getUser(url);
+
