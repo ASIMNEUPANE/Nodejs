d7d1786a0413baf565dfab2a0ec16b6249885f57
ASIMNEUPANE <124865108+ASIMNEUPANE@users.noreply.github.com>
2023-08-15 17:39:07 +0545

    learn routing

--- express/express.js
+++ express/express.js
@@ -1,9 +1,18 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World Asim')
-})
+app.get("/", function (req, res) {
+  res.send("Hello World Asim");
+});
+app.post("/post", function (req, res) {
+  res.send("task complet");
+});
+app.delete("/delete", function (req, res) {
+  res.send("Hello World ");
+});
+// app.get("/", function (req, res) {
+//   res.send("Hello World Asim");
+// });
 
-app.listen(3000)
-console.log("app running on port 3000")
+app.listen(3000);
+console.log("app running on port 3000");
